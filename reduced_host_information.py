d2977ae18b410223a03eb1a06dda0e29393f5373
Ethan Wilson <ehwilson.business@gmail.com>
2025-03-04 15:30:20 -0500

    added check to see if user wanted all or some information. created list of names and dictionary of queries for the get_some_host_facts function

--- reduced_host_information.py
+++ reduced_host_information.py
@@ -3,17 +3,16 @@ import sys
 import signal
 import requests
 import urllib3
-import json
+import xlsxwriter
 import os
 import pandas as pd
 from dotenv import load_dotenv
 
+
 #signal handler for interupts
-#FIXME  add cleanup code
 def signal_handler(sig, frame):
     print('\nInterupt Caught. Exiting program...\n')
     sys.exit(0)
-
 signal.signal(signal.SIGINT, signal_handler)
 
 #check for csv files existence. delete if file exists and generate new file
@@ -26,7 +25,9 @@ def check_file(filepath):
         print("'host_information.csv' does not exist. Generating new file: 'host_information.csv'\n")
 
 class API_data:
+
     def __init__(self, TOKEN):
+        self.all_flag = 1
         self.host_nums = []
         self.host_names = {}
         self.headers = {
@@ -48,19 +49,71 @@ class API_data:
                 next_url = 'https://ansible.vai.org:8043' + r_json['next']
                 self.get_host_nums(next_url)
             else:
-                self.get_host_facts()
+                if self.all_flag == 1:
+                    self.get_all_host_facts()
+                else:
+                    self.get_some_host_facts()
+
         else:
             print(f"\nError: {r.status_code}). Exiting program...\n")
             sys.exit(1)
 
-    def get_host_facts(self):
+    
+    def get_some_host_facts(self):
         count_hosts = 0
-        col_num = 0
-
-        #FIXME: might be a better way to handle existing files
-        #delete old host_facts file if it exists
-        csv_file = 'host_information.csv'
+        csv_file = 'host_information_some.csv'
         self.check_file(csv_file)
+
+        #FIXME index the processor to only show the first item. ansible_processor[0] = CPU name
+
+        server_mac_addresses = []
+        column_labels = ['Server Name', 'Product Serial', 'Chassis Serial', 'Model', 'Chassis Vendor', 'System Vendor', 'Memory', 'Processor (CPU)', 'Processor Cores', 'Processor Count']
+        api_queries = {'ansible_nodename':'',      
+                      'ansible_product_serial':'',
+                      'ansible_chassis_serial':'',
+                      'ansible_product_name':'',
+                      'ansible_chassis_vendor':'',
+                      'ansible_system_vendor':'',
+                      'ansible_memory_mb':'',
+                      'ansible_processor':'',
+                      'ansible_processor_cores':'',
+                      'ansible_processor_count':''}
+        
+        """
+        row, col = 0, 0
+        workbook = xlsxwriter.Workbook('host_information_some.xlsx')
+        worksheet = workbook.add_worksheet()
+        for label in column_labels:
+            worksheet.write(row, col, label)
+            col += 1
+
+        
+
+        for host_no in self.host_nums:
+            url = f'https://ansible.vai.org:8043/api/v2/hosts/{host_no}/ansible_facts'
+            r = requests.get(url, headers=self.headers, verify=False)
+
+            if r.status_code == 200:
+                r_json = r.json()
+                for key in api_queries:
+                    try:
+                        api_queries[key] = r_json[key]
+                    except KeyError:
+                        api_queries[key] = 'N/A'
+
+                count_hosts += 1
+
+            else:
+                print(f"\nError: {r.status_code}). Exiting program...\n")
+                sys.exit(1)
+                
+        print(f'\nNumber of hosts saved: {count_hosts}')"""
+
+        
+    def get_all_host_facts(self):
+        count_hosts = 0
+        csv_file = 'host_information_all.csv'
+        check_file(csv_file)
         
 
         #retrieving information for each host
@@ -69,13 +122,9 @@ class API_data:
             url = f'https://ansible.vai.org:8043/api/v2/hosts/{host_no}/ansible_facts'
             r = requests.get(url, headers=self.headers, verify=False)
 
-            #check if status code is good for retrieval
+            #convert response object to json
             if r.status_code == 200:
-                r_json = r.json()   #convert response object to json
-                #print(json.dumps(r_json, indent=2))
-
-                #collect reduced information from the json response
-                
+                r_json = r.json()
                 self.host_names[host_no] = r_json['ansible_nodename']
                 df = pd.json_normalize(r_json, sep=' ')
 
@@ -88,7 +137,7 @@ class API_data:
                 df = df.T
 
                 df.to_csv(csv_file, mode='a')
-                print(f"Successfully saved {server} data to host_information.csv")
+                print(f"Successfully saved {server} data to {csv_file}")
                 count_hosts += 1
 
             else:
@@ -99,33 +148,25 @@ class API_data:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 #suppress warnings and load environment variables
 urllib3.disable_warnings()
 load_dotenv()
 
-#FIXME: add check to see if user wants whole information or just the reduced information
 #check if environmental variable called 'TOKEN' exists. 
 if 'TOKEN' in os.environ:
     TOKEN = os.getenv('TOKEN')
-    print("Process Starting...\n")
-    data = API_data(TOKEN=TOKEN)
-    data.get_hosts_nums( url = 'https://ansible.vai.org:8043/api/v2/hosts/')
+
+    all_flag = input("Do you want to retrieve all host information? (y/n): ")
+    if all_flag.lower() == 'n':
+        data = API_data(TOKEN=TOKEN)
+        data.all_flag = 0
+    else:
+        data = API_data(TOKEN=TOKEN)
+        data.all_flag = 1
+
+    print("\nProcess Starting...\n")
+    data.get_host_nums( url = 'https://ansible.vai.org:8043/api/v2/hosts/')
     print('Process Ended Successfully\n')
 else:
     print("'TOKEN' does not exist as an evironment variable.")
     print("Initialize an environmental variable called 'TOKEN' with your bearer token. Rerun the script upon completion.\n")
-
-#
