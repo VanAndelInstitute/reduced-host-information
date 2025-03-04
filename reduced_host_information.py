078425bce5ae0009fe24407ffa194fb62c20228f
Ethan Wilson <ehwilson.business@gmail.com>
2025-03-04 11:41:20 -0500

    changed variable name of host_lst to host_nums for readability purposes

--- reduced_host_information.py
+++ reduced_host_information.py
@@ -8,14 +8,14 @@ from dotenv import load_dotenv
 
 class API_data:
     def __init__(self, TOKEN):
-        self.host_lst = []
+        self.host_nums = []
         self.host_names = {}
         self.headers = {
             "Authorization": f"Bearer {TOKEN}",
             "Content-Type": "application/json"
         }
 
-    #add hosts to host_lst from each page of the API
+    #add host numbers to host_nums from each page of the API
     def get_hosts(self, url):
         r = requests.get(url, headers=self.headers, verify=False)
         if r.status_code == 200:
@@ -23,7 +23,7 @@ class API_data:
             df = pd.json_normalize(r_json['results']) #flatten the data to pull the host ids
             hosts = df['id']
             for host_no in hosts:
-                self.host_lst.append(host_no)
+                self.host_nums.append(host_no)
             
             #if there is a next page, recurse with the url held in the 'next' field
             if r_json['next']:
@@ -31,6 +31,19 @@ class API_data:
                 self.get_hosts(next_url)
             else:
                 self.get_facts()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     #retrieves the information of every host and appends it to a file called host_information.csv
     def get_facts(self):
@@ -47,12 +60,23 @@ class API_data:
             print("'host_information.csv' does not exist. Generating new file: 'host_information.csv'\n")
         
         #retrieving information for each host
-        for host_no in self.host_lst:
+        for host_no in self.host_nums:
+
+            #create URL based on host number and request the information
             url = f'https://ansible.vai.org:8043/api/v2/hosts/{host_no}/ansible_facts'
             r = requests.get(url, headers=self.headers, verify=False)
+
+            #check if status code is good for retrieval
             if r.status_code == 200:
-                r_json = r.json()
+                r_json = r.json()   #convert response object to json
+                print(json.dumps(r_json, indent=2))
+
+                #collect reduced information from the json response
+                
                 self.host_names[host_no] = r_json['ansible_nodename']
+                
+
+
                 df = pd.json_normalize(r_json, sep=' ')
 
                 #add hostname to each column of information, then trans
@@ -67,6 +91,20 @@ class API_data:
                 print(f"Successfully saved {server} data to host_information.csv")
                 count_hosts += 1
         print(f'\nNumber of hosts saved: {count_hosts}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #suppress warnings and load environment variables
@@ -84,3 +122,4 @@ else:
     print("'TOKEN' does not exist as an evironment variable.")
     print("Initialize an environmental variable called 'TOKEN' with your bearer token. Rerun the script upon completion.\n")
 
+#
