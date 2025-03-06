f943d77e6df959a795a74eb6c2ce52311982ea8a
Ethan Wilson <ehwilson.business@gmail.com>
2025-03-06 16:37:48 -0500

    migrated interface filtering and and mac address querying to outside of the class in its own function

--- reduced_host_information.py
+++ reduced_host_information.py
@@ -10,10 +10,13 @@ from dotenv import load_dotenv
 
 
 def signal_handler(sig, frame):
+
     print('\nInterupt Caught. Exiting program...\n')
+
     sys.exit(0)
 
 def check_file(filepath):
+
     if os.path.exists(filepath) and os.path.isfile(filepath):
         print(f"{filepath} already exists. This file will be removed and a new one will be generated.\n")
         os.remove(filepath)
@@ -23,11 +26,28 @@ def check_file(filepath):
 
 def reduce_interfaces(interfaces):
     skip_lst = ['v','d']
+
     for interface in interfaces:
-        if interface[0] in skip_lst:             #remove virtual ethernet interfaces
+        if interface[0] in skip_lst:
             interfaces.remove(interface)
+
     return interfaces
 
+def query_interfaces(server_mac_addresses, reduced_ifs, r_json):
+    valid_ifs = []
+
+    for inf in reduced_ifs:                    
+        mac_query = f'ansible_{inf}'
+        try:
+            mac_address = r_json[mac_query]['macaddress']
+            server_mac_addresses[inf] = mac_address
+            valid_ifs.append(inf)
+        except:
+            continue
+    
+    return valid_ifs
+
+############################################################################################################
 
 class API_data:
 
@@ -63,6 +83,7 @@ class API_data:
             print(f"\nError: {r.status_code}). Exiting program...\n")
             sys.exit(1)
 
+##############################################################
     
     def get_some_host_facts(self):
         count_hosts = 0
@@ -70,7 +91,9 @@ class API_data:
         check_file(csv_file)
 
         #FIXME index the processor to only show the first item. ansible_processor[0] = CPU name
+        #create a list of column names and query dict values
         interfaces = []
+        reduced_ifs = []
         column_labels = ['Server Name', 'Product Serial', 'Chassis Serial', 'Model', 'Chassis Vendor', 'System Vendor', 'Memory', 'Processor (CPU)', 'Processor Cores', 'Processor Count']
         api_queries = {'ansible_nodename':'',      
                       'ansible_product_serial':'',
@@ -79,28 +102,24 @@ class API_data:
                       'ansible_chassis_vendor':'',
                       'ansible_system_vendor':'',
                       'ansible_memory_mb':'',
-                      'ansible_processor':'',
+                      'ansible_processor[0]':'',
                       'ansible_processor_cores':'',
                       'ansible_processor_count':''}
         
+        #iterate through all hosts
         for host_no in self.host_nums:
             url = f'https://ansible.vai.org:8043/api/v2/hosts/{host_no}/ansible_facts'
             r = requests.get(url, headers=self.headers, verify=False)
 
+            #
             if r.status_code == 200:
-                r_json = r.json()
-                server_mac_addresses = {}
-                interfaces = r_json['ansible_interfaces']
-                interfaces = reduce_interfaces(interfaces)
+                server_mac_addresses = {}   #reset the server mac address dict
+                r_json = r.json()           #convert the response into a json object
+                interfaces = r_json['ansible_interfaces']       #retrieve all the interface names
+                reduced_ifs = reduce_interfaces(interfaces)      #filter out the virtual and docker interfaces
 
-                for interface in interfaces:
-                    mac_query = f'ansible_{interface}'
-                    try:
-                        mac_address = r_json[mac_query]['macaddress']
-                        server_mac_addresses[interface] = mac_address
-                    except:
-                        interfaces.remove(interface)
-                        continue
+                #get the server interfaces and final reduced interfaces
+                valid_ifs = query_interfaces(server_mac_addresses, reduced_ifs, r_json)
             
                 if server_mac_addresses:
                     print(r_json['ansible_nodename'])
@@ -122,6 +141,7 @@ class API_data:
                 
         print(f'\nNumber of hosts saved: {count_hosts}')
 
+##############################################################
         
     def get_all_host_facts(self):
         count_hosts = 0
@@ -159,6 +179,7 @@ class API_data:
                 
         print(f'\nNumber of hosts saved: {count_hosts}')
 
+############################################################################################################
 
 signal.signal(signal.SIGINT, signal_handler)
 
