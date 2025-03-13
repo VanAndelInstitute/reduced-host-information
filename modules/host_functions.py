47ad476bf29ec3046ced7d6b33bfbb12d0fae046
Ethan Wilson <ehwilson.business@gmail.com>
2025-03-13 15:33:49 -0400

    moved some things from main to other functions such as the URL. started structure for the create csv file in host_functions.py. split the flag checking between the get_curr_page_hosts functino and main function

--- modules/host_functions.py
+++ modules/host_functions.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import requests
 
 #check if environmental variable called 'TOKEN' exists. 
 def check_token():
@@ -10,6 +12,7 @@ def check_token():
         print("Exiting program...\n")
         exit(1)
 
+
 def check_file(filepath):
     if os.path.exists(filepath) and os.path.isfile(filepath):
         print(f"{filepath} already exists. This file will be removed and a new one will be generated.\n")
@@ -17,6 +20,7 @@ def check_file(filepath):
         print(f"Generating new file: '{filepath}\n")
     else:
         print(f"{filepath} does not exist. Generating new file: {filepath}\n")
+
 
 def init_api_data_structure(token, API_data_var):
     all_flag = input("Do you want to retrieve all host information? (y/n): ")
@@ -29,6 +33,7 @@ def init_api_data_structure(token, API_data_var):
 
     return data
 
+
 def reduce_interfaces(interfaces):
     skip_lst = ['v', 'd']
 
@@ -40,17 +45,91 @@ def reduce_interfaces(interfaces):
 
     return interfaces
 
-def query_interfaces(server_mac_addresses, reduced_ifs, r_json):
-    valid_ifs = []
 
+def query_interfaces(server_mac_addresses, reduced_ifs, r_json):
     for intf in reduced_ifs:                    
         mac_query = f'ansible_{intf}'
         try:
             mac_address = r_json[mac_query]['macaddress']
             server_mac_addresses[intf] = mac_address
-            valid_ifs.append(intf)
         except:
             continue
 
-    return valid_ifs
+####################################################################################
 
+def get_all_host_facts(self):
+    count_hosts = 0
+    csv_file = 'host_information_all.csv'
+    check_file(csv_file)
+
+    #retrieving information for each host
+    for host_no in self.host_nums:
+        #create URL based on host number and request the information
+        url = f'https://ansible.vai.org:8043/api/v2/hosts/{host_no}/ansible_facts'
+        r = requests.get(url, headers=self.headers, verify=False)
+
+        #convert response object to json
+        if r.status_code == 200:
+            r_json = r.json()
+            try:
+                self.get_curr_host_facts(self, response=r_json, csv_file=csv_file, host_no=host_no)
+            except:
+                print("Error: Couldn't save host information")
+        else:
+            print(f"\nError: {r.status_code}). Exiting program...\n")
+            sys.exit(1)
+            
+    print(f'\nNumber of hosts saved: {count_hosts}')
+
+
+#FIXME: FIX THE FORMATTING. AND APPEND THE INFORMATION IN THE END. THIS IS LAST PRIORITY 
+def get_curr_host_facts(self, response, csv_file, host_no):
+
+    self.host_names[host_no] = response['ansible_nodename']
+    df = pd.json_normalize(response, sep=' ')
+
+    #add hostname to each column of information, then trans
+    server = f'{self.host_names[host_no]}'
+    host_nodename = [server] * len(df.columns)  #to add a host nodename each row of information
+    df.loc[-1] = host_nodename 
+    df.index += 1
+    df.sort_index()
+    df = df.T
+
+    df.to_csv(csv_file, mode='a')
+    print(f"Successfully saved {server} data to {csv_file}")
+    count_hosts += 1
+
+
+def get_some_curr_host_facts(host_names, response, host_no):
+
+    api_queries = {'ansible_nodename':'',      
+        'ansible_product_serial':'',
+        'ansible_chassis_serial':'',
+        'ansible_product_name':'',
+        'ansible_chassis_vendor':'',
+        'ansible_system_vendor':'',
+        "ansible_memory_mb":'',
+        'ansible_processor':'',
+        'ansible_processor_cores':'',
+        'ansible_processor_count':''}
+
+    host_names[host_no] = response['ansible_nodename']
+    for k in api_queries.keys():
+        try:
+            if k == 'ansible_memory_mb':
+                api_queries[k] = response[k]['real']['total']
+            elif k == "ansible_processor":
+                api_queries[k] = response[k][2]
+            else:
+                api_queries[k] = response[k]
+        except:
+            print("Error: " + k)
+
+    return api_queries
+
+
+def create_csv():
+    csv_file = 'host_information_some.csv'
+    check_file(csv_file)
+    column_labels = ['Server Name', 'Product Serial', 'Chassis Serial', 'Model', 'Chassis Vendor', 'System Vendor', 'Memory', 'Processor (CPU)', 'Processor Cores', 'Processor Count']
--- modules/reduced_host_information.py
+++ modules/reduced_host_information.py
@@ -1,4 +1,4 @@
-import modules.host_functions as host_functions
+import host_functions
 import os
 import sys
 import csv
@@ -28,18 +28,22 @@ class API_data:
             "Content-Type": "application/json"
         }
 
+######################################################################################
+
     #GOOD
     #initialize list of all hosts numbers
-    def get_host_nums(self, url):
+    def get_host_nums(self):
         #check if status code is good, retrieve all host numbers. exit if otherwise
+        url = 'https://ansible.vai.org:8043/api/v2/hosts/'
         r = requests.get(url, headers=self.headers, verify=False)
         if r.status_code == 200:
             r_json = r.json()
             self.get_curr_page_hosts(response = r_json)
-
         else:
             print(f"\nError: {r.status_code}). Exiting program...\n")
             sys.exit(1)
+
+#######################################################################################
 
     #GOOD
     #get all the hosts from the curretly "visible" page
@@ -56,107 +60,20 @@ class API_data:
         if response['next']:
             next_url = 'https://ansible.vai.org:8043' + response['next']
             self.get_host_nums(next_url)
-        else:
-            if self.all_flag == 1:
-                host_functions.get_all_host_facts()
-            else:
-                host_functions.get_some_host_facts()
 
-    #FIXME: FIX THE FORMATTING. AND APPEND THE INFORMATION IN THE END. THIS IS LAST PRIORITY 
-    def get_curr_host_facts(self, response, csv_file, host_no):
+#######################################################################################
 
-        self.host_names[host_no] = response['ansible_nodename']
-        df = pd.json_normalize(response, sep=' ')
-
-        #add hostname to each column of information, then trans
-        server = f'{self.host_names[host_no]}'
-        host_nodename = [server] * len(df.columns)  #to add a host nodename each row of information
-        df.loc[-1] = host_nodename 
-        df.index += 1
-        df.sort_index()
-        df = df.T
-
-        df.to_csv(csv_file, mode='a')
-        print(f"Successfully saved {server} data to {csv_file}")
-        count_hosts += 1
-
-    
-    def get_some_curr_host_facts(self, response, host_no):
-
-        api_queries = {'ansible_nodename':'',      
-            'ansible_product_serial':'',
-            'ansible_chassis_serial':'',
-            'ansible_product_name':'',
-            'ansible_chassis_vendor':'',
-            'ansible_system_vendor':'',
-            "ansible_memory_mb":'',
-            'ansible_processor':'',
-            'ansible_processor_cores':'',
-            'ansible_processor_count':''}
-
-        #get current host name
-        self.host_names[host_no] = response['ansible_nodename']
-        for k in api_queries.keys():
-            try:
-                if k == 'ansible_memory_mb':
-                    api_queries[k] = response[k]['real']['total']
-                elif k == "ansible_processor":
-                    api_queries[k] = response[k][2]
-                else:
-                    api_queries[k] = response[k]
-            except:
-                print("Error: " + k)
-
-        return api_queries
-
-    
-    def get_all_host_facts(self):
-        count_hosts = 0
-        csv_file = 'host_information_all.csv'
-        host_functions.check_file(csv_file)
-
-        #retrieving information for each host
-        for host_no in self.host_nums:
-            #create URL based on host number and request the information
-            url = f'https://ansible.vai.org:8043/api/v2/hosts/{host_no}/ansible_facts'
-            r = requests.get(url, headers=self.headers, verify=False)
-
-            #convert response object to json
-            if r.status_code == 200:
-                r_json = r.json()
-                try:
-                    self.get_curr_host_facts(self, response=r_json, csv_file=csv_file, host_no=host_no)
-                except:
-                    print("Error: Couldn't save host information")
-            else:
-                print(f"\nError: {r.status_code}). Exiting program...\n")
-                sys.exit(1)
-                
-        print(f'\nNumber of hosts saved: {count_hosts}')
 
     def get_some_host_facts(self):
         count_hosts = 0
-        csv_file = 'host_information_some.csv'
-        host_functions.check_file(csv_file)
-
         interfaces = []
         reduced_ifs = []
-        column_labels = ['Server Name',
-                        'Product Serial',
-                        'Chassis Serial',
-                        'Model',
-                        'Chassis Vendor',
-                        'System Vendor',
-                        'Memory',
-                        'Processor (CPU)',
-                        'Processor Cores',
-                        'Processor Count']
-
 
         #iterate through all hosts
         for host_no in self.host_nums:
 
-            api_queries = {}
+            api_queries = {}    #reset api_queries to empty
+            server_mac_addresses = {}   #reset the server mac address dictionary
 
             #get URL for specific host
             url = f'https://ansible.vai.org:8043/api/v2/hosts/{host_no}/ansible_facts'
@@ -166,19 +83,17 @@ class API_data:
             if r.status_code == 200:
                 r_json = r.json()
 
-                #get the facts listed in the api_queries for the current node
-                api_queries = self.get_some_curr_host_facts(r_json, host_no)
+                #get facts for the current node
+                api_queries = host_functions.get_some_curr_host_facts(self.host_names, r_json, host_no)
 
-                server_mac_addresses = {}   #reset the server mac address dictionary
-
-                #retrieve all the interface names
+                #get all interface names for current node
                 interfaces = r_json['ansible_interfaces']
 
                 #filter out the virtual and docker interfaces
                 reduced_ifs = host_functions.reduce_interfaces(interfaces)
 
                 #get the server interfaces and final reduced interfaces
-                valid_ifs = host_functions.query_interfaces(server_mac_addresses, reduced_ifs, r_json)
+                host_functions.query_interfaces(server_mac_addresses, reduced_ifs, r_json)
 
 
             #print statements for server information
@@ -201,17 +116,24 @@ class API_data:
 
 ############################################################################################################
 
+#initialize signal handler and supress warnings
 signal.signal(signal.SIGINT, signal_handler)
-
-#suppress warnings and load environment variables
 urllib3.disable_warnings()
 load_dotenv()
 
-
+#retrieve API token and initialize API_data data structure with token
 TOKEN = host_functions.check_token()
 data = host_functions.init_api_data_structure(token=TOKEN, API_data_var=API_data)
-url = 'https://ansible.vai.org:8043/api/v2/hosts/'
 
+#let user know process is starting
 print("\nProcess Starting...\n")
-data.get_host_nums(url = url)
+
+#get every host number from the API
+data.get_host_nums()
+if data.all_flag == 1:
+    host_functions.get_all_host_facts()
+else:
+    host_functions.get_some_host_facts()
+
+#if here then process was successful
 print('Process Ended Successfully\n')
